555a55b7549a67240661b6606ef15eb1d3eda12d
김도훈 <dohun1109@naver.com>
2023-10-03 18:54:24 +0900

    10/03 SpringStudy

--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -7,9 +7,11 @@ import hello.core.order.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationContextBasicFindTest {
 
@@ -22,15 +24,56 @@ class ApplicationContextBasicFindTest {
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
 //        System.out.println("memberService = " + memberService);
 //        System.out.println("memberService.getClass() = " + memberService.getClass());
-        
     }
-
     @Test
     @DisplayName("이름 없이 타입으로만 조회")
     void  findBeanByType() {
         MemberService memberService = ac.getBean( MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
+    @Test
+    @DisplayName("구체 타입으로 조회")  //역활(Interface)과 구현(Impl)을 나눠야 하고, 역활에 의존(DIP)해야하야 한다.
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean( MemberServiceImpl.class); //이 코드는 구현에 의존하기 때문에 별로 좋은 코드는 아니다.
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        //ac.getBean("xxxxx",MemberService.class);
+        //MemberService xxxxx = ac.getBean("xxxxx",MemberService.class); //NoSuchBeanDefinitionException -> 이용가능한 빈이 없다!(찾을 수 없다)
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+
+    }
 
      
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
