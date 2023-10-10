398aaedd58dfd8d587997ac661c86634c9a79c34
김도훈 <dohun1109@naver.com>
2023-10-10 21:31:42 +0900

    study2023/10/10

--- core/src/test/java/hello/core/singleton/SingleTonTest.java
+++ core/src/test/java/hello/core/singleton/SingleTonTest.java
@@ -4,6 +4,8 @@ import hello.core.member.MemberService;
 import hello.core.order.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -47,7 +49,7 @@ public class SingleTonTest {
         //참조값이 같은 것을 확인
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
-        
+
         // singletonService1 == singletonService2
         assertThat(singletonService1).isSameAs(singletonService2);
         //isSameAs == 참조 값을 비교 
@@ -55,11 +57,51 @@ public class SingleTonTest {
         singletonService1.logic();
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void SpringContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-//    public static void main(String[] args) {
-////        SingletonService singletonService = new SingletonService();
-//
-//    }
+
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+
+        assertThat(memberService1).isSameAs(memberService2);
+
+
+        /* 스프링 컨테이너 덕분에 고객의 요청이 올 때 마다 객체를 생성하는 것이 아니라, 이미 만들어진 객체를 공유해서 효율적으로 재사용
+            할 수있다. 참고로 스프링의 기본 빈 등록 방식은 싱글톤이지만, 싱글톤 방식만 지원하는 것은 아니다. 요청할 때 마다 새로운 객체를
+            생성해서 반환하는 기능도 제공한다.(Ex http request Life cycle 에 맞춰서 bean life cycle 을 맞추거나 이럴이럴 때)
+             -> 자세한건 뒤에 빈스코프에서
+         */
+
+        
+
+    }
+
+
+    /*
+     *  싱글톤 방식의 주의점
+     * 싱글톤 패턴이든, 스프링 같은 싱글톤 컨테이너 사용하든, 객체 인스턴스를 하나만 생성해서 공유하는 싱글톤 방식은 여러 클라이언트가 하나의 객체 인스턴스를 공유하기 때문에 싱글톤 객체는 상태를 유지(stateful)하게 설계하면 안된다.
+     *
+     * 무상태(stateless)로 설계해야한다.
+     * - 특정 클라이언트에 의존적인 필드가 있으면 안된다.
+     * - 특정 클라이언트가 값을 변경할 수 있는 필드가 있으면 안된다!
+     * - 가급적 읽기만(get o , set x) 가능해야 한다.
+     * - 필드 대신에 자바에서 공유되지 않는, 지역변수, 파라미터, ThreadLocal 등을 사용해야 한다.
+     * 스프링 빈의 필드에 공유 값을 설정하면 정말 큰 장애가 발생할 수 있다.
+     */
+
 
 }
 
